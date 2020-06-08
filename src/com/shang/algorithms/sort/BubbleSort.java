885660abdae61a410abdda3b52c0fa400d736d58
shangwei <shangwei@tuhu.cn>
2020-06-08 20:00:24 +0800

    添加冒泡排序

--- src/com/shang/algorithms/sort/BubbleSort.java
+++ src/com/shang/algorithms/sort/BubbleSort.java
@@ -8,25 +8,32 @@ import java.util.Arrays;
  */
 public class BubbleSort {
 
-	/**
-	 * 单向冒泡排序
-	 *
-	 * @param array
-	 */
-	public static void sort(int[] array) {
-		for (int i = 0; i < array.length - 1; i++) {
+    /**
+     * 单向冒泡排序
+     *
+     * @param array
+     */
+    public static void sort(int[] array) {
+        for (int i = 1; i <= array.length - 1; i++) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[i]){
+                    swap(array,i,j);
+                }
+            }
+        }
+    }
 
-		}
+    public static void swap(int[] array, int i, int j) {
+        array[i] = array[i] ^ array[j];
+        array[j] = array[i] ^ array[j];
+        array[i] = array[i] ^ array[j];
+    }
 
 
-	}
-
-
-
-	public static void main(String[] args) {
-		int[] array = {2, 12, 4, 3, 22, 32, 7, 5, 5, 8};
-		sort(array);
-		System.out.println(Arrays.toString(array));
-	}
+    public static void main(String[] args) {
+        int[] array = {2, 12, 4, 3, 22, 32, 7, 5, 5, 8};
+        sort(array);
+        System.out.println(Arrays.toString(array));
+    }
 
 }
